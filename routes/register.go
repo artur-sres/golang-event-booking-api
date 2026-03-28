80f7053832ad9e3979ca6b143d09614c242810ff
Artur <asr.engsoft@gmail.com>
2026-03-28 13:47:40 -0300

    Add event registration

---
+++ routes/register.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/artur-sres/golang-event-booking-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userID := context.GetInt64("userID")
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"Message": "Could not parse event ID."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+
+	err = event.Register(userID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register for event"})
+		return
+	}
+	
+	context.JSON(http.StatusOK, gin.H{"message": "Registration successful", "event": event})
+
+}
+
+func cancelRegistration(context *gin.Context) {
+
+}
