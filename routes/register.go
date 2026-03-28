b34169a792398024dfe181099a486a59a2d6d14c
Artur <asr.engsoft@gmail.com>
2026-03-28 14:13:34 -0300

    Add cancel registration

--- routes/register.go
+++ routes/register.go
@@ -27,11 +27,28 @@ func registerForEvent(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register for event"})
 		return
 	}
-	
+
 	context.JSON(http.StatusOK, gin.H{"message": "Registration successful", "event": event})
 
 }
 
 func cancelRegistration(context *gin.Context) {
+	userID := context.GetInt64("userID")
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"Message": "Could not parse event ID."})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventID
+
+	err = event.CancelRegistration(userID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to cancel registration"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Registration canceled", "event": event})
 
 }
